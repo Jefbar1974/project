a4d7204f53684e456741a501e8314acbbb691020
jefbar1974 <jefbar@gmail.com>
2022-09-15 22:06:14 -0300

    verificando ex1

---
+++ aula7-js/exemplos-js/ex1/somar.js
@@ -0,0 +1,23 @@
+
+
+console.log("Fora da Função")
+
+
+function somar()  {
+  
+
+let valorUm = document.querySelector("#numero1").value
+let valorDois = document.querySelector("#numero2").value
+let valorTres = document.querySelector("#numero3").value
+
+   
+
+    let resultado = Number(valorUm) + Number(valorDois) + Number(valorTres)
+
+   let divresultado = document.querySelector("#resultado")
+   divresultado.innerHTML = resultado
+
+
+
+
+}
